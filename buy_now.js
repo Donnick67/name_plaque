e94bcb90b209eb3153ec95de6a9588688258efd1
udechibueze <donnick67@gmail.com>
2022-05-24 15:51:53 +0100

    first commit

---
+++ buy_now.js
@@ -0,0 +1,12 @@
+buyNow = (buysubmit) => {
+    return "Input letters";
+    alert('enter custom letters')
+
+    if (value > 15) {
+        window.alert("number of available letter exceeded")
+    } else {
+        window.alert("Success")
+    }
+}    
+
+
